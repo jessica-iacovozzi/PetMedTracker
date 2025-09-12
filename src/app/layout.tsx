927933764283845a06b6006ec665af5e9ff4f05b
jessica-iacovozzi <iacovozzi.jessica@gmail.com>
2025-09-12 23:45:03 +0000

    chore: upgrade Next.js to v15, add ESLint config and remove unused Script import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import { TempoInit } from "@/components/tempo-init";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import Script from "next/script";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
